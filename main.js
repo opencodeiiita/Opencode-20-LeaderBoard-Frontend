8373672aa0e0218fe96ddf1e457636e033aedec0
JhX <iit2019134@iiita.ac.in>
2020-01-22 22:09:34 +0530

    Added Participant Photo

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myArr = JSON.parse(this.responseText);
         document.getElementById('leaderboard').innerHTML = 
-        '<thead><tr><th>Rank</th><th>Github ID</th><th>Points</th></tr></thead><tbody></tbody>'; //same as original structure
+        '<thead><tr><th>Rank</th><th>&nbsp;   Photo</th>&nbsp; <th>Github ID</th><th>Points</th></tr></thead><tbody></tbody>'; //same as original structure
         document.getElementById('loader').innerHTML = '';
         addToTable(myArr);
         addTop5images(myArr);
@@ -22,11 +22,12 @@ function addToTable(arr) {
     for(i = 5; i < arr.length; i++) {
             name=arr[i].username;
             points= arr[i].points;
+			img = arr[i].img;
             /*if (0<=i && i<3){
-              var markup = "<tr><td><strong>"+ (i + 1) + "</strong></td><td><strong>" + "&nbsp;" + name + "</strong></td><td><strong>" + "&nbsp;" + points + "</strong></td></tr>";
+              var markup = "<tr><td><strong>"+ (i + 1) +"</strong></td><td><strong>" +'<img src="' +img+ '" alt="Italian Trulli" style="width:50px; height:50px;">' + "</strong></td><td><strong>" + "&nbsp;" + name + "</strong></td><td><strong>" + "&nbsp;" + points + "</strong></td></tr>";
             }
             else{*/
-              var markup = "<tr><td>"+ (i + 1) + "</td><td> " + "&nbsp;" + name + "</td><td> " + "&nbsp;" + points + "</td></tr>";
+              var markup = "<tr><td>"+ (i + 1) +"</strong></td><td><strong>" +'<img src="'+img+ '" alt="Italian Trulli" style="width:50px; height:50px;">' + "</td><td> " + "&nbsp;" + name + "</td><td> " + "&nbsp;" + points + "</td></tr>";
             //}
             $("table tbody").append(markup);
     }
